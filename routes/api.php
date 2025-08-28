b6e0d0aed0d1a7d515b351f5fdd63560ed12243a
Anthony19cr <1920anto@gmail.com>
2025-08-27 18:27:54 -0600

    Configurar Sanctum y CORS: ajustes en Kernel, migraciones y configs

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', fn() => auth()->user());
+});
