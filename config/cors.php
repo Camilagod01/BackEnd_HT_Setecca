7a673f576e48caf6bc8bf1539bee5be9fed57666
Anthony19cr <1920anto@gmail.com>
2025-08-29 13:54:00 -0600

    actualización de CORS para permitir front en localhost:5173 y 8080

--- config/cors.php
+++ config/cors.php
@@ -1,21 +1,18 @@
 <?php
 
 return [
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://planillas.local', 'http://localhost:5173'],
+    // Permite el front de Vite y el front servido por Apache
+    'allowed_origins' => [
+        'http://localhost:5173',   // Vite dev server
+        'http://localhost:8080',   // Front bajo Apache/Laragon
+    ],
 
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
